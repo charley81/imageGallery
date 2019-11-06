d895f22736f64d22851a6c38c63332cf7517301a
chrisharley81 <chrisharley81@gmail.com>
2019-11-06 17:28:43 -0500

    change main image on click functionality

--- dist/script.js
+++ dist/script.js
@@ -4,3 +4,17 @@ const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('.btn');
 const thumbnails = document.querySelector('.thumbnails');
 
+for (let i = 1; i <= 5; i++) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', './images/pic' + [i] + '.jpg');
+  thumbnails.appendChild(newImage);
+
+  newImage.onclick = function (e) {
+    const imageSrc = e.target.getAttribute('src');
+    displayImage(imageSrc);
+
+    function displayImage() {
+      currentImage.setAttribute('src', imageSrc);
+    }
+  }
+}
