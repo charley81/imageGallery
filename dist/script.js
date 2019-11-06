3ff90275ddf1f85386720cc78e603022cabfd161
chrisharley81 <chrisharley81@gmail.com>
2019-11-06 17:58:44 -0500

    css for overlay & button onclick lighten/darken overlay

--- dist/script.js
+++ dist/script.js
@@ -18,3 +18,16 @@ for (let i = 1; i <= 5; i++) {
     }
   }
 }
+
+btn.onclick = function () {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
