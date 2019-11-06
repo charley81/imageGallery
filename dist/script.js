9a7cbdc29fd1c7366dd462758790011a8b1b09e9
chrisharley81 <chrisharley81@gmail.com>
2019-11-06 16:28:36 -0500

    UI vars

--- dist/script.js
+++ dist/script.js
@@ -4,8 +4,3 @@ const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('.btn');
 const thumbnails = document.querySelector('.thumbnails');
 
-for (let i = 1; i <= 5; i++) {
-  const newImg = document.createElement('img');
-  newImg.setAttribute('src', './images/pic' + [i] + '.jpg');
-  thumbnails.appendChild(newImg);
-}
