05a70f766a6c4d625c92800db1a3d3481296c86f
chrisharley81 <chrisharley81@gmail.com>
2019-11-05 20:58:27 -0500

    thumbnails css

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,11 @@
-body {
-  font - family: 'Montserrat', sans - serif;
+const imgBox = document.querySelector('.img-box');
+const currentImage = document.querySelector('.current-image');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.btn');
+const thumbnails = document.querySelector('.thumbnails');
+
+for (let i = 1; i <= 5; i++) {
+  const newImg = document.createElement('img');
+  newImg.setAttribute('src', './images/pic' + [i] + '.jpg');
+  thumbnails.appendChild(newImg);
 }
